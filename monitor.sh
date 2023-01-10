504a5ef558029bcfef5ce06144216a56fb90625b
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-10 10:16:34 +0100

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# este script monitorea el uso de CPU y Memeoria
+# script monitorea, numero de conexiones establecidas, % uso de CPU y de Memoria
+# (c) hacking y seguridad .com 2023
 
 while :
 do
   cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
   memUsage=$(free -m | awk '/Mem/{print $3}')
-  echo "CPU Usage: $cpuUsage%"
-  echo "Memory Usage: $memUsage MB"
+  echo "Uso de CPU: $cpuUsage%"
+  echo "Uso de Memoria: $memUsage MB"
+  echo "Conexiones: " && netstat -plan|grep :$1 | awk {'print $5'} | cut -d: -f 1 | sort | uniq -c
   sleep 3
+  echo "### www.hackingyseguridad.com ###"
 done
+
