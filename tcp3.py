8018afca3f8f2f8dea6f389d86f85f41a098caaf
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-22 20:57:12 +0100

    Update tcp3.py

--- tcp3.py
+++ tcp3.py
@@ -1,22 +1,24 @@
-# sport = random.randint(1024,65535)
-dport = 80
-# ip_src = '192.168.23.131'
-ip_dst = '81.17.241.142'
+# Ataque amplificacion enviando una peticion http y flags TCP: SYN + ACK:PSH
+# Peticion HTTP GET, con handshake TCP, em 3 solo pasos. Flags TCP: SYN + ACK:PSH
+# hackingyseguridad.com 2023
 
-# SYN     
-ip=IP(src=ip_src, dst=ip_dst)
-SYN=TCP(sport=sport, dport=dport, flags='S', seq=1000)
+import sys
+import socket
+from scapy.all import *
+
+# SYN
+ip=IP(dst="104.16.88.120")
+SYN=TCP( dport=80, flags='S', seq=1000)
 SYNACK=sr1(ip/SYN)
 
-# ACK          
-ACK=TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)
+# ACK
+ACK=TCP(dport=80, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)
 send(ip/ACK)
 
 # GET request
 req =  "GET / HTTP/1.1\r\n"
-req += "Host: " + ip_dst + "\r\n"
 # Other headers ...
 req += "\r\n"
 
-DATA = TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)/req
+DATA = TCP(dport=80, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)/req
 ANS = sr1(DATA) # this is probably going to be only an ACK segment, next one should be your HTTP response
