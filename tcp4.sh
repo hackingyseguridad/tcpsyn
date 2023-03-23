336421dcf99be9aac8b94fec542bb8aaec123546
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-23 19:40:16 +0100

    Create tcp4.sh

---
+++ tcp4.sh
@@ -0,0 +1,9 @@
+curl 'http://104.16.88.120/' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: es-ES,es;q=0.9,en;q=0.8' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'Connection: keep-alive' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36' \
+  --compressed \
+  --insecure
